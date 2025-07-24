8dd36c93bf28a5aa452c1fa4eaf9f6fb674eac38
Souilles <206949127+Souilles@users.noreply.github.com>
2025-07-25 00:43:25 +0200

    Added a loop to play again

--- guess-the-number.py
+++ guess-the-number.py
@@ -1,20 +1,36 @@
 import random
-
-number=random.randint(1,100)
 guess=0
+play="yes"
 
 def user_guess():
-    guess=int(input("Enter your guess:"))
+    guess=int(input("Enter your guess: "))
     return guess
 
-print("Welcome. I'm thinking of a number between 1 and 100, can you guess it ?")
-guess=user_guess()
-while guess!=number:
-    if guess>number:
-        print("Too high !")
-        guess=user_guess()
-    else:
-        print("Too low !")
-        guess=user_guess()
+def play_again():
+    play=input("Do you want to play again ?")
+    play=play.lower()
+    while play not in["yes", "no"]:
+        play=input("please answer with yes or no: ")
+    return play
 
-print("Congrats, you win ! :)")
+while play=="yes":
+
+    number=random.randint(1,100)
+    print("I'm thinking of a number between 1 and 100, can you guess it ?")
+    guess=user_guess()
+    while guess!=number:
+        if guess>number:
+            print("Too high !")
+            guess=user_guess()
+        else:
+            print("Too low !")
+            guess=user_guess()
+
+    print("Congrats, you win ! :)")
+
+    play=play_again()
+
+if play=="no":
+    print("Okay ! Have a nice day !")
+
+
